1d8b39cbc2b6a07e89a34b4b778ca26a75bee61d
Abbos AKrmv <abbosbeksika@gmail.com>
2026-05-02 18:30:13 +0500

    saved all

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://postgres:123@localhost:5432/fauth_db', echo=True)
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:123@localhost:5432/fauth_db')
+engine = create_engine(DATABASE_URL, echo=True)
 
 Base = declarative_base()
 
